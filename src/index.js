cdd1027a1bdf5ffc06ba0c5f92cb70f48d330554
ichigor <igor.moiano@hotmail.com>
2018-10-21 22:49:34 -0300

    Adicionado uso de push notifications

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import React, { Component } from 'react';
 //   View,
 // } from 'react-native';
 
+import OneSignal from 'react-native-onesignal';
+
 import { ApolloProvider } from 'react-apollo';
 
 import client from './apollo/client';
@@ -16,7 +18,26 @@ import Chat from './pages/Chat';
 
 export default class App extends Component {
   componentDidMount() {
-    // console.tron.log();
+    OneSignal.init("19704337-6377-4414-8511-31c08633ae59");
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  onReceived(notification) {
+    console.log("Notification received:", notification);
+  }
+
+  onOpened(result) {
+    console.log('Message:', result.notification.payload.body);
+    console.log('Data:', result.notification.payload.additionalData);
+    console.log('isActive:', result.notification.isAppInFocus);
+    console.log('Result:', result);
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device);
   }
 
   render() {
